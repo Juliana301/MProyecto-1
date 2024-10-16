1fdfc46e29c5a3c3ee22e5c16d75ce5359f2c875
Juliana301 <julianagonzalez3015@gmail.com>
2024-10-16 16:49:53 -0600

    act

--- ambiente semanas/View/Login/inicioSesion.php
+++ ambiente semanas/View/Login/inicioSesion.php
@@ -1,12 +1,16 @@
+<?php
+    include_once '../../Controller/LoginController.php';
+?>
+
 <!doctype html>
 <html lang="en">
 
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>SeoDash Free Bootstrap Admin Template by Adminmart</title>
+    <title>Proyecto Web Miércoles</title>
     <link rel="shortcut icon" type="image/png" href="images/seodashlogo.png" />
-    <link rel="stylesheet" href="css/styles.min.css" />
+    <link rel="stylesheet" href="../css/styles.min.css" />
 </head>
 
 <body>
@@ -21,36 +25,40 @@
                         <div class="card mb-0">
                             <div class="card-body">
                                 <a href="home.php" class="text-nowrap logo-img text-center d-block py-3 w-100">
-                                    <img src="images/logo-light.svg" alt="">
+                                    <img src="../images/logo-light.svg" alt="">
                                 </a>
-                                <p class="text-center">Registro de Usuarios</p>
+
+                                <p class="text-center">Inicio de sesión</p>
+
                                 <form action="" method="POST">
+
                                     <div class="mb-3">
-                                        <label for="exampleInputtext1" class="form-label">Identificación</label>
-                                        <input type="text" class="form-control" id="txtIdentificacion" name="txtIdentificacion"
-                                            >
-                                    </div>
-                                    <div class="mb-3">
-                                        <label for="exampleInputEmail1" class="form-label">Nombre</label>
-                                        <input type="text" class="form-control" id="txtNombre" name="txtNombre">
-                                    </div>
-                                    <div class="mb-4">
-                                        <label for="exampleInputPassword1" class="form-label">Correo Electrónico</label>
-                                          <input type="text" class="form-control" id="txtCorreo" name="txtCorreo"
-                                    </div>
-                                    <div class="mb-4">
-                                        <label for="exampleInputPassword1" class="form-label">Contraseña</label>
-                                         <input type="text" class="form-control" id="txtContrasenna" name="txtContrasenna">
+                                        <label class="form-label">Correo Electrónico</label>
+                                        <input type="email" class="form-control" id="txtCorreo" name="txtCorreo">
                                     </div>
 
-                                     <input type="submit" class="btn btn-primary w-100 py-8 fs-4 mb-4" value="Procesar"
-                                     
+                                    <div class="mb-4">
+                                        <label class="form-label">Contraseña</label>
+                                        <input type="password" class="form-control" id="txtContrasenna" name="txtContrasenna">
+                                    </div>
+
+                                    <div class="d-flex align-items-center justify-content-between mb-4">
+                                        <div class="form-check">
+
+                                        </div>
+                                        <a class="text-primary fw-bold" href="recuperarAcceso.php">Recuperar acceso</a>
+                                    </div>
+
+                                    <input type="submit" class="btn btn-primary w-100 py-8 fs-4 mb-4" value="Procesar"
+                                        id="btnIniciarSesion" name="btnIniciarSesion">
 
                                     <div class="d-flex align-items-center justify-content-center">
-                                        <p class="fs-4 mb-0 fw-bold">Already have an Account?</p>
-                                        <a class="text-primary fw-bold ms-2" href="inicioSesion.php">Iniciar sesión</a>
+                                        <p class="fs-4 mb-0 fw-bold">No tienes una cuenta?</p>
+                                        <a class="text-primary fw-bold ms-2" href="registrarUsuario.php">Registrarse</a>
                                     </div>
+
                                 </form>
+
                             </div>
                         </div>
                     </div>
@@ -58,7 +66,7 @@
             </div>
         </div>
     </div>
-    
+
     <script src="js/jquery.min.js"></script>
     <script src="js/bootstrap.bundle.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/iconify-icon@1.0.8/dist/iconify-icon.min.js"></script>
--- ambiente semanas/View/Login/recuperarAcceso.php
+++ ambiente semanas/View/Login/recuperarAcceso.php
@@ -1,5 +1,5 @@
 <?php
-include_once '../Controller/LoginController.php';
+    include_once '../../Controller/LoginController.php';
 ?>
 
 <!doctype html>
@@ -8,9 +8,9 @@ include_once '../Controller/LoginController.php';
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Proyecto Web Miercoles</title>
+    <title>SeoDash Free Bootstrap Admin Template by Adminmart</title>
     <link rel="shortcut icon" type="image/png" href="images/seodashlogo.png" />
-    <link rel="stylesheet" href="css/styles.min.css" />
+    <link rel="stylesheet" href="../css/styles.min.css" />
 </head>
 
 <body>
@@ -25,34 +25,26 @@ include_once '../Controller/LoginController.php';
                         <div class="card mb-0">
                             <div class="card-body">
                                 <a href="home.php" class="text-nowrap logo-img text-center d-block py-3 w-100">
-                                    <img src="images/logo-light.svg" alt="">
+                                    <img src="../images/logo-light.svg" alt="">
                                 </a>
-                                <p class="text-center">Registro De Usuarios</p>
-                                
+
+                                <p class="text-center">Recuperar el acceso</p>
+
                                 <form action="" method="POST">
+
                                     <div class="mb-3">
                                         <label class="form-label">Correo Electrónico</label>
-                                        <input type="email" class="form-control" id="txtCorreo" name="txtCorreo" >
-                                    </div>
-                                    <div class="mb-4">
-                                        <label class="form-label">Contraseña</label>
-                                        <input type="password" class="form-control" id="txtContraseña" name="txtContraseña">
-                                    </div>
-                                    <div class="d-flex align-items-center justify-content-between mb-4">
-                                        <div class="form-check">
-
-                                        </div>
-                                        <a class="text-primary fw-bold" href="recuperarAcceso.php">Recuperar acceso</a>
+                                        <input type="email" class="form-control" id="txtCorreo" name="txtCorreo">
                                     </div>
 
                                     <input type="submit" class="btn btn-primary w-100 py-8 fs-4 mb-4" value="Procesar"
-                                    id="btnIniciarSesion" name="btnIniciarSesion">
+                                    id="btnRecuperarAcceso" name="btnRecuperarAcceso">
                                     
                                     <div class="d-flex align-items-center justify-content-center">
-                                        <p class="fs-4 mb-0 fw-bold">New to SeoDash?</p>
-                                        <a class="text-primary fw-bold ms-2" href="registrarUsuario.php">Crear una
-                                            cuenta</a>
+                                        <p class="fs-4 mb-0 fw-bold">Ya tienes una cuenta?</p>
+                                        <a class="text-primary fw-bold ms-2" href="inicioSesion.php">Inicia sesión</a>
                                     </div>
+
                                 </form>
                             </div>
                         </div>
@@ -61,7 +53,7 @@ include_once '../Controller/LoginController.php';
             </div>
         </div>
     </div>
-
+    
     <script src="js/jquery.min.js"></script>
     <script src="js/bootstrap.bundle.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/iconify-icon@1.0.8/dist/iconify-icon.min.js"></script>
